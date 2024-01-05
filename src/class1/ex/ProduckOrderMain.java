23069ddf5b64977322e03127ccf22fa4a977422d
Sejun0910 <choisejun99@gmail.com>
2024-01-05 20:08:10 +0900

    클래스와 데이터

---
+++ src/class1/ex/ProduckOrderMain.java
@@ -0,0 +1,31 @@
+package class1.ex;
+
+public class ProduckOrderMain {
+    public static void main(String[] args) {
+        ProductOrder[] orders = new ProductOrder[3];
+
+        ProductOrder order1 = new ProductOrder();
+        order1.produckName = "김치";
+        order1.price = 2000;
+        order1.quantity = 2;
+        orders[0] = order1;
+
+        ProductOrder order2 = new ProductOrder();
+        order2.produckName = "두부";
+        order2.price = 3000;
+        order2.quantity = 3;
+        orders[1] = order2;
+
+        ProductOrder order3 = new ProductOrder();
+        order3.produckName = "고기";
+        order3.price = 8000;
+        order3.quantity = 1;
+        orders[2] = order3;
+
+        int totalAmount = 0;
+        for (ProductOrder order : orders) {
+            System.out.println("상품명 : " + order.produckName + ", 가격 : " + order.price + ", 수량 : " + order.quantity);
+        }
+
+    }
+}
